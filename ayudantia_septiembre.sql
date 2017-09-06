ee6f255425eb968419563594c0b5bac20a3f9083
PedroRoblero <31290836+PedroRoblero@users.noreply.github.com>
2017-09-06 17:01:32 -0300

    clase 06-09

---
+++ ayudantia_septiembre.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-09-2017 a las 21:57:16
+-- Versión del servidor: 10.1.26-MariaDB
+-- Versión de PHP: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ayudantia_septiembre`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+CREATE TABLE `alumno` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `rut` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ramo`
+--
+
+CREATE TABLE `ramo` (
+  `id` int(11) NOT NULL,
+  `nombre_ramo` varchar(255) NOT NULL,
+  `nota` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `ramo`
+--
+ALTER TABLE `ramo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `ramo`
+--
+ALTER TABLE `ramo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ formulario.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+
+<div class="container"> 
+	<div class="row">
+		<div class="col-md-6 well">
+			<h3>Alumno</h3>
+			<form action="datos.php" method="POST">
+				Nombre: <br>
+				<input type="text" name="nombre"> <br>
+				Apellido <br>
+				<input type="text" name="apellido"> <br>
+				RUT: <br>
+				<input type="text" name="rut"> <br> <br>
+				<input type="hidden" name="alumno_formulario">
+				<input type="submit" class="btn btn-success"> 
+			</form>
+		</div>
+
+		<div class="col-md-6 well">
+		<h3>Ramo</h3>
+			<form action="datos.php" method="POST">
+    			Ramo: <br>
+				<input type="text" name="ramo"> <br>
+				Nota: <br>
+				<input type="text" name="nota"> <br> <br>
+				<input type="hidden" name="ramo_formulario">
+				<input type="submit" class="btn btn-danger">
+			</form>
+		</div>
+	</div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>	
+<script type="text/javascript" src="js/bootstrap.js" </script>
+	
+</body>
+</html>
